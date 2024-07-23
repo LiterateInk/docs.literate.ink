c9fdb3bc0a0d87f59872bb7761f7094a90050d68
Mikkel RINGAUD <mikkel.almonteringaud@legrand.com>
2024-07-23 15:25:40 +0200

    feat: add libs on home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { HomeLayout } from 'fumadocs-ui/home-layout';
 import { LinkItemType } from 'fumadocs-ui/layout';
+import React from 'react';
+import { HomeCard } from '@/components/HomeCard';
 
 const navLinks: LinkItemType[] = [
   {
@@ -13,28 +15,81 @@ const navLinks: LinkItemType[] = [
     url: 'https://literate.ink/discord',
     active: 'nested-url',
   },
-]
+];
 
 export default function HomePage() {
   return (
-    <HomeLayout nav={{ title: <p>LiterateInk</p> }} links={navLinks}>
-      <main className="mx-auto max-w-6xl pt-8 px-4 lg:px-0">
-        <section className="w-full overflow-x-hidden">
-          <div className="relative flex flex-col lg:items-center z-10">
-            <h1 className="text-4xl lg:text-5xl font-bold pt-20 lg:pt-40 mb-6">
-              A home for our documentations
-            </h1>
-            <p className="lg:text-center text-lg max-w-2xl text-gray-600 dark:text-gray-400 mb-10">
-              Comprehensive documentation for all our libraries with API references and detailed guides.
-            </p>
-            <div className="flex flex-row gap-4 items-center">
-              <a href="https://github.com/LiterateInk" className="inline-flex items-center justify-center font-medium transition-colors focus-visible:outline-none disabled:pointer-events-none bg-secondary/60 hover:bg-accent shadow-background/20 h-11 px-6 rounded-full border border-bg-secondary/40">
-                GitHub
-              </a>
+    <HomeLayout nav={{ title: <p>LiterateInk</p> }} links={navLinks} githubUrl="https://github.com/LiterateInk">
+      <div className="min-h-screen grid" style={{ "gridTemplateRows": "1fr auto"}}>
+        <main className="overflow-hidden relative w-full pt-8 px-4 lg:px-0 space-y-16 pb-48">
+          <section className="mx-auto w-full max-w-6xl overflow-x-hidden">
+            <div className="relative flex flex-col lg:items-center z-10">
+              <h1 className="text-4xl lg:text-5xl font-bold pt-20 mb-6">
+                A home for our documentations
+              </h1>
+              <p className="lg:text-center text-lg max-w-2xl text-gray-600 dark:text-gray-400 mb-10">
+                Comprehensive documentation for all our libraries with API references and detailed guides.
+              </p>
+
+              <div className="flex flex-row gap-4 items-center">
+                <a href="https://github.com/LiterateInk" className="inline-flex items-center justify-center font-medium transition-colors focus-visible:outline-none disabled:pointer-events-none bg-secondary/60 hover:bg-accent shadow-background/20 h-11 px-6 rounded-full border border-bg-secondary/40">
+                  GitHub
+                </a>
+              </div>
             </div>
+          </section>
+
+          <section className="mx-auto w-full max-w-6xl flex flex-wrap justify-center items-stretch gap-8">
+            <HomeCard
+              name='Pawnote'
+              description='A purrfect API wrapper for PRONOTE'
+            />
+            <HomeCard
+              name='Pawdirecte'
+              description='A purrfect API wrapper for EcoleDirecte'
+            />
+            <HomeCard
+              name='Educawtable'
+              description='An awmazing API wrapper for Educartable'
+            />
+            <HomeCard
+              name='Meowdle'
+              description='A purrfect API wrapper for Moodle'
+            />
+            <HomeCard
+              name='Turbawself'
+              description='An awmazing API wrapper for Turboself'
+            />
+            <HomeCard
+              name='WebRestaw'
+              description='An awmazing API wrapper for WebResto'
+            />
+            <HomeCard
+              name='PawRD'
+              description='An awmazing API wrapper for ARD GEC'
+            />
+            <HomeCard
+              name='Crowous'
+              description='A wrapper for the mobile API of Les Crous'
+            />
+            <HomeCard
+              name='Pawnilim'
+              description='A purrfect API wrapper for Unilim'
+            />
+          </section>
+
+          <div className='transition-colors absolute bg-[hsl(var(--hue),60,40)]/30 dark:bg-[hsl(var(--hue),50,60)]/60 blur-[100px] w-full h-[150px] rounded-full -bottom-12 right-0 left-0' />
+        </main>
+
+        <footer className='py-6 bg-neutral-100 dark:bg-neutral-900 border-t border-neutral-200 dark:border-neutral-800 w-full flex justify-between px-6'>
+          <div className="flex flex-col justify-center">
+            <p className='text-neutral-600 dark:text-neutral-400 text-sm'>© {new Date().getFullYear()} LiterateInk</p>
           </div>
-        </section>
-      </main>
+
+          <a className='dark:hidden' href="https://www.netlify.com"> <img src="https://www.netlify.com/v3/img/components/netlify-color-bg.svg" alt="Deploys by Netlify" /> </a>
+          <a className='hidden dark:block' href="https://www.netlify.com"> <img src="https://www.netlify.com/v3/img/components/netlify-color-accent.svg" alt="Deploys by Netlify" /> </a>
+        </footer>
+      </div>
     </HomeLayout>
   );
 }
---
+++ components/HomeCard.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { Github } from "lucide-react";
+import Link from "next/link";
+
+const HUES = {
+  PAWNOTE: 140,
+  TURBAWSELF: 21,
+  PAWDIRECTE: 235,
+  WEBRESTAW: 35,
+  CROWOUS: 358,
+  PAWNILIM: 351,
+  EDUCAWTABLE: 190,
+  PAWRD: 170,
+  MEOWDLE: 28
+} as const;
+
+export const HomeCard: React.FC<{ name: string, description: string }> = ({ name, description }) => {
+  const hue = HUES[name.toUpperCase() as keyof typeof HUES];
+
+  const setHue = () => document.documentElement.style.setProperty('--hue', hue.toString());
+  const resetHue = () => document.documentElement.style.removeProperty('--hue');
+
+  return (
+    <Link
+      href={`/${name.toLowerCase()}`}
+      onPointerEnter={() => setHue()}
+      onPointerLeave={() => resetHue()}
+      className='overflow-hidden relative z-10 flex justify-between border rounded-xl pl-5 pr-14 gap-3 py-4 max-w-[320px] w-full dark:border-neutral-800 bg-neutral-100/40 dark:bg-neutral-900/50 backdrop-blur-xl hover:border-[hsl(var(--hue),60,70)]/50 hover:bg-[hsl(var(--hue),100,90)]/20 dark:hover:border-[hsl(var(--hue),30,50)]/50 dark:hover:bg-[hsl(var(--hue),30,9)] transition-colors'
+      // style={{ borderColor: `hsl(${hue} 76 90 / .2)` }}
+    >
+      <div className="flex flex-col gap-1">
+        <p className='font-semibold'>{name}</p>
+        <p className="text-sm opacity-75">{description}</p>
+      </div>
+
+      <div className="absolute bottom-0 left-0 h-16 w-24 blur-[80px] rounded-full" style={{ backgroundColor: `hsl(${hue} 76 50 / .2)`}} />
+      <div className="absolute bottom-0 right-0 h-16 w-24 blur-[80px] rounded-full" style={{ backgroundColor: `hsl(${hue} 38 45 / .9)`}} />
+    </Link>
+  )
+}
